ae32c6a8857cf3a410ead94787c8c075cc27175f
Fernando Dalai <ferdalaixd@gmail.com>
2023-06-08 12:57:01 -0600

    Objects

---
+++ Terminal_text.js
@@ -0,0 +1,8 @@
+// Output of text
+console.log("Hello World");
+
+// Input of text
+const prompt = require('prompt-sync')(); //For this you need to install 'prompt-sync' using npm
+
+let name = prompt("Enter your name: ");
+console.log("Hello " + name);
